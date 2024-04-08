7d28b38932d0d749d0e6fa856843dc44cae74630
nain1984edibusa <edibusa_1991@hotmail.com>
2024-04-08 10:28:05 -0500

    Server Config - ms-administracion-server-config

---
+++ ms-administracion-server-config/src/test/java/gob/coretributario/msadministracionserverconfig/MsAdministracionServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package gob.coretributario.msadministracionserverconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAdministracionServerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
