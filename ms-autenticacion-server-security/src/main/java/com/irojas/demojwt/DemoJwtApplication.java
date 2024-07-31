56a1808caed9d66b8733067fb22f1ec8385ac799
nain1984edibusa <edibusa_1991@hotmail.com>
2024-07-31 08:37:22 -0500

    ms auth spring security refactoring

--- ms-autenticacion-server-security/src/main/java/com/irojas/demojwt/DemoJwtApplication.java
+++ ms-autenticacion-server-security/src/main/java/com/irojas/demojwt/DemoJwtApplication.java
@@ -2,7 +2,10 @@ package com.irojas.demojwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoJwtApplication {
 
@@ -11,3 +14,4 @@ public class DemoJwtApplication {
 	}
 
 }
+
