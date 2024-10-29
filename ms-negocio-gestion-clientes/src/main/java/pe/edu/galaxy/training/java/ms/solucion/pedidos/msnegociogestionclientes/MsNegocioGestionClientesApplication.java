618f8d834a343f64a1188e3bb4b33194ccd985ca
Eduardo Bustillos <edibusa_1991@hotmail.com>
2024-10-28 21:19:46 -0500

    refactoring ms-negocio-cliente

--- ms-negocio-gestion-clientes/src/main/java/pe/edu/galaxy/training/java/ms/solucion/pedidos/msnegociogestionclientes/MsNegocioGestionClientesApplication.java
+++ ms-negocio-gestion-clientes/src/main/java/pe/edu/galaxy/training/java/ms/solucion/pedidos/msnegociogestionclientes/MsNegocioGestionClientesApplication.java
@@ -2,7 +2,9 @@ package pe.edu.galaxy.training.java.ms.solucion.pedidos.msnegociogestionclientes
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsNegocioGestionClientesApplication {
 
