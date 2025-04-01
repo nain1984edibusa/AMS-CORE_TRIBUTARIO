bd4ab1cd1a7ecf3433fa22f555d65c0f64aa8df7
Eduardo Bustillos <edibusa_1991@hotmail.com>
2025-04-01 15:11:40 -0500

    circuit break - ms-inscripcion_vta_client

--- ms-inscripcion-vta-client/src/main/java/gob/mdmq/coretributario/msnegociogestiontributo/services/PostsServiceImpl.java
+++ ms-inscripcion-vta-client/src/main/java/gob/mdmq/coretributario/msnegociogestiontributo/services/PostsServiceImpl.java
@@ -7,7 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import gob.mdmq.coretributario.msnegociogestiontributo.dto.PostsDto;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.reactor.circuitbreaker.operator.CircuitBreakerOperator;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
@@ -16,25 +18,24 @@ import reactor.util.retry.Retry;
 public class PostsServiceImpl implements PostsService {
 
 	private final WebClient webClient;
+	private final CircuitBreaker circuitBreaker;
 
-	public PostsServiceImpl(WebClient.Builder webClientBuilder) {
-//		this.webClient = webClientBuilder.baseUrl("http://localhost:9992").build();
-		//.baseUrl("http://localhost:8080/ms-negocio-gestion-inscripcion-vta").build();
+	public PostsServiceImpl(WebClient.Builder webClientBuilder, CircuitBreakerRegistry registry) {
 		// Realiza la llamada al microservicio `ms-negocio-gestion-inscripcion-vta`
-		this.webClient = webClientBuilder
-							.baseUrl("http://localhost:8887/ms-negocio-gestion-inscripcion-vta").build();
-		
+		this.webClient = webClientBuilder.baseUrl("http://localhost:8887/ms-negocio-gestion-inscripcion-vta").build();
+		this.circuitBreaker = registry.circuitBreaker("cbObtenerInscripcion");
 	}
 
 	public Flux<PostsDto> obtenerInformacion() {
 		return webClient.get()
 				.uri("/api/v1/inscripcion-vta") // Endpoint del microservicio de destino
 				.retrieve()
-				.bodyToFlux(PostsDto.class)
-				.timeout(Duration.ofSeconds(60)) // Tiempo máximo permitido // Tiempo de espera para esta operación
-				.onErrorResume(throwable -> {
-					System.err.println("Error al realizar la solicitud: " + throwable.getMessage());
-					return Flux.empty(); // Retorna un flujo vacío en caso de error
-				});
+                .bodyToFlux(PostsDto.class)
+                .timeout(Duration.ofSeconds(60))
+                .transformDeferred(CircuitBreakerOperator.of(circuitBreaker))
+                .onErrorResume(throwable -> {
+                    System.err.println("Error al realizar la solicitud: " + throwable.getMessage());
+                    return Flux.empty(); // Fallback en caso de error
+                });
 	}
 }
