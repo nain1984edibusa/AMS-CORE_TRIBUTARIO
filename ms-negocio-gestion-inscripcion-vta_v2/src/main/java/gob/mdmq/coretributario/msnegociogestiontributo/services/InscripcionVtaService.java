0f1f995de17fd2c7358a880ba52d4d03c77f5c87
Eduardo Bustillos <edibusa_1991@hotmail.com>
2025-04-01 15:42:53 -0500

    eliminar - ms-negocio-gestion-inscripcion-vta2

--- ms-negocio-gestion-inscripcion-vta_v2/src/main/java/gob/mdmq/coretributario/msnegociogestiontributo/services/InscripcionVtaService.java
+++
@@ -1,9 +0,0 @@
-package gob.mdmq.coretributario.msnegociogestiontributo.services;
-
-import gob.mdmq.coretributario.msnegociogestiontributo.dto.InscripcionVtaDTO;
-
-public interface InscripcionVtaService extends GenericService<InscripcionVtaDTO>{
-
-	
-
-}
